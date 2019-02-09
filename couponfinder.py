3fb275d2305fc34c679e01d2aae37e3388386dcd
denzilly <btimmer313@gmail.com>
2019-02-09 12:50:35 +0100

    added some extra failsafes and stuff, write to csv

--- couponfinder.py
+++ couponfinder.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from random import *
 
+import csv
+
 import time
 
 ############LIST OF XPATHS##############
@@ -31,6 +33,7 @@ def main(rangemin,rangemax):
     codes = []
 
     wts = 0.9
+    wtl = 3
 
 
     driver = webdriver.Firefox()
@@ -46,12 +49,25 @@ def main(rangemin,rangemax):
     driver.find_element_by_xpath(pstcd).send_keys("6222ah")
     driver.find_element_by_xpath(huis).send_keys("77")
     driver.find_element_by_xpath(next).click()
-
     time.sleep(wts)
+
 
     driver.find_element_by_xpath(adbtn).click()
 
     time.sleep(wts)
+
+    #in case shop is closed
+    try:
+        driver.find_element_by_xpath('//*[@id="order_time_select"]').click()
+        driver.find_element_by_xpath('//*[@id="order_time_select"]').send_keys('1')
+        driver.find_element_by_xpath('//*[@id="start-order-button"]').click()
+    except:
+        print("store is open")
+
+
+
+
+    time.sleep(wtl)
 
     for x in range(rangemin, rangemax):
         try:
@@ -59,21 +75,31 @@ def main(rangemin,rangemax):
             driver.find_element_by_xpath(coupbtn).click()
             time.sleep(wts)
         except:
+            print("error")
+            time.sleep(wtl)
             continue
         try:
             ht = driver.find_element_by_xpath(coupmsg).text
             driver.find_element_by_xpath(coupclose).click()
         except:
-            codes.append(x)
-            print(str(x) + driver.find_element_by_xpath(coupdesc).text)
-            driver.find_element_by_xpath(coupdel).click()
-            continue
+            try:
+                codes.append((str(x) + driver.find_element_by_xpath(coupdesc).text))
+                print(str(x) + driver.find_element_by_xpath(coupdesc).text)
+                driver.find_element_by_xpath(coupdel).click()
+                continue
+            except:
+                time.sleep(wtl)
+                continue
+
         time.sleep(wts)
+    print(codes)
+    with open('coupons.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        for row in codes:
+            writer.writerow([row])
+
+    csvFile.close()
+    driver.close()
 
 
-
-
-
-
-
-main(0,10000)
+main(10000,20000)
